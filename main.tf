2ba0ddce323ac5536a45dde5dde113cdeac066ec
Thomas Smallwood <thomasinio@hotmail.com>
2023-05-31 18:54:26 +0100

    add infracost_breakdown

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_organizations_organization" "org" {
   # aws_service_access_principals = [
   #   "cloudtrail.amazonaws.com"
   # ]
-  feature_set = "ALL"
+  # feature_set = "ALL"
 }
 
 # deploy AFT
